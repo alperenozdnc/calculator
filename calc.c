8203a57bd0eaa9c44e8498834db5847ddcf60048
alperenozdnc <alperenozdincc@gmail.com>
2025-05-06 03:18:13 +0300

    feat: validate exp

--- calc.c
+++ calc.c
@@ -5,13 +5,6 @@
 typedef enum { PLUS = '+', MINUS = '-', TIMES = '*', DIVIDE = '/' } Operations;
 typedef enum { LEFT = '(', RIGHT = ')' } Parentheses;
 
-struct AllowedCharacters {
-    char parentheses[2];
-    char operations[4];
-    char space;
-    int digits[10];
-};
-
 void get_expression(char *expression);
 bool validate_expression(char expression[]);
 
@@ -19,19 +12,6 @@ int main() {
     char expression[100] = {0};
 
     get_expression(expression);
-
-    const Parentheses left_paren = LEFT;
-    const Parentheses right_paren = RIGHT;
-    const Operations plus = PLUS;
-    const Operations minus = MINUS;
-    const Operations times = TIMES;
-    const Operations divide = DIVIDE;
-
-    struct AllowedCharacters allowed_characters = {
-        .parentheses = {left_paren, right_paren},
-        .operations = {plus, minus, times, divide},
-        .space = ' ',
-        .digits = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}};
 
     const bool IS_EXP_VALID = validate_expression(expression);
 
@@ -44,8 +24,94 @@ int main() {
 
 void get_expression(char *expression) {
     printf("enter expression: ");
-    fgets(expression, sizeof(expression), stdin);
+    fgets(expression, 100, stdin);
     expression[strlen(expression) - 1] = '\0';
 }
 
-bool validate_expression(char expression[]) { return false; }
+bool validate_expression(char expression[]) {
+    const Parentheses left_paren = LEFT;
+    const Parentheses right_paren = RIGHT;
+    const Operations plus = PLUS;
+    const Operations minus = MINUS;
+    const Operations times = TIMES;
+    const Operations divide = DIVIDE;
+    const char space = ' ';
+
+    char allowed_characters[] = {
+        left_paren, right_paren, plus, minus, times, divide, space, '1', '2',
+        '3',        '4',         '5',  '6',   '7',   '8',    '9',   '0'};
+
+    bool result = true;
+
+    int operation_count = 0;
+    int parentheses_count = 0;
+    int left_paren_count = 0;
+    int right_paren_count = 0;
+
+    char exp_no_spaces[strlen(expression)] = {};
+
+    for (int i = 0; i < strlen(expression); i++) {
+        char c = expression[i];
+        bool char_is_allowed = false;
+
+        for (int j = 0; j < strlen(allowed_characters); j++) {
+            char allowed_c = allowed_characters[j];
+
+            if (allowed_c == c) {
+                char_is_allowed = true;
+
+                if (c == space)
+                    continue;
+
+                int l = strlen(exp_no_spaces);
+
+                exp_no_spaces[l] = c;
+                exp_no_spaces[l + 1] = '\0';
+            }
+        }
+
+        if (!char_is_allowed) {
+            printf("'%c' is not an allowed character\n", expression[i]);
+
+            return false;
+        }
+    }
+
+    if (strlen(exp_no_spaces) % 2 == 0) {
+        printf("missing operator(s) and/or operand(s)\n");
+        return false;
+    }
+
+    for (int i = 0; i < strlen(exp_no_spaces); i++) {
+        char c = exp_no_spaces[i];
+
+        if (c == plus || c == minus || c == times || c == divide) {
+            if (i == 0 || i == strlen(exp_no_spaces) - 1) {
+                printf("expression can't start or end with an operator\n");
+                return false;
+            }
+
+            operation_count++;
+        }
+
+        if (c == left_paren) {
+            left_paren_count++;
+            parentheses_count++;
+        } else if (c == right_paren) {
+            right_paren_count++;
+            parentheses_count++;
+        }
+    }
+
+    if (parentheses_count % 2 != 0 || left_paren_count != right_paren_count) {
+        printf("a parenthesis wasn't closed\n");
+        return false;
+    }
+
+    if (operation_count == 0) {
+        printf("there has to be at least one operation\n");
+        return false;
+    }
+
+    return result;
+}
